40fd12430a04349cd17861fe3bb1fdefc6192950
sreehari7212 <sreeharicr999@GMAIL.COM>
2023-07-15 13:54:28 +0530

    2nd report for user

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views, user_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('home/',views.home,name='home'),
     path('Content_add/',user_views.Content_add,name='Content_add'),
     path('Contentt/', user_views.Contentt, name='Contentt'),
@@ -25,9 +25,10 @@ urlpatterns = [
     # path('report/',views.reportt,name='report'),
     path('person/',views.person,name='person'),
     path('logout_view/',views.logout_view,name='logout_view'),
-    path('login_view/',views.login_view,name='login_view'),
+    path('',views.login_view,name='login_view'),
     path('user_profile/', user_views.user_profile, name='user_profile'),
     path('personal_report/', user_views.personal_report, name='personal_report'),
+    path('personal_report2/', user_views.personal_report2, name='personal_report2'),
     path('generate_pdf3/', views.generate_pdf3, name='generate_pdf3'),
     # path('generate_pdf4/', user_views.generate_pdf4, name='generate_pdf4'),
     path('attendance_pdf/', views.attendance_pdf, name='attendance_pdf'),
--- app/user_views.py
+++ app/user_views.py
@@ -9,7 +9,7 @@ from xhtml2pdf import pisa
 from app.forms import ContentForm
 from app.models import Content, Employee, Detected_in, Detected_out
 from io import BytesIO
-@login_required(login_url='/app/login/')
+# @login_required(login_url='/app/login/')
 def Content_add(request):
     form = ContentForm()
     u = request.user
@@ -25,13 +25,13 @@ def Content_add(request):
         form = ContentForm()
     return render(request, 'app/content_add.html', {'form': form})
 
-@login_required(login_url='/app/login/')
+# @login_required(login_url='/app/login/')
 def Contentt(request):
     n = Content.objects.filter(user=request.user)
     return render(request, 'app/content.html', {'Content': n})
 
 
-@login_required(login_url='/app/login/')
+# @login_required(login_url='/app/login/')
 def user_profile(request):
     u = request.user
     profile = Employee.objects.filter(user=u)
@@ -83,6 +83,116 @@ def personal_report(request):
             return response
 
         return render(request, 'admintemp/personal_report.html', context)
+
+
+from django.db.models import Min, Max
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+from io import BytesIO
+import datetime
+import django.template.loader as loader
+from xhtml2pdf import pisa
+
+from django.db.models import Min, Max
+
+from django.db.models import Min, Max
+
+from django.db.models import Min, Max
+
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='/app/login/')
+def personal_report2(request):
+    if request.method == 'GET':
+        user = request.user
+        employee = Employee.objects.get(user=user)
+        start_date = request.GET.get('start_date', None)
+        end_date = request.GET.get('end_date', None)
+        if start_date and end_date:
+            start_date_formatted = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+            end_date_formatted = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+
+            attendance_data = []
+
+            det_list_in = Detected_in.objects.filter(emp_id=employee,
+                                                     entry__date__range=(start_date_formatted, end_date_formatted))
+            det_list_out = Detected_out.objects.filter(emp_id=employee,
+                                                       out__date__range=(start_date_formatted, end_date_formatted))
+
+            date_range = set(det_list_in.values_list('entry__date', flat=True)) | set(
+                det_list_out.values_list('out__date', flat=True))
+
+            # Process each date for the employee
+            for date in sorted(date_range):
+                det_in = det_list_in.filter(entry__date=date).order_by('entry').first()
+                det_out = det_list_out.filter(out__date=date).order_by('out').last()
+
+                if det_in and det_out:
+                    total_time = det_out.out - det_in.entry
+                    total_time_str = str(datetime.timedelta(seconds=total_time.seconds))
+                    attendance_data.append({
+                        'employee_id': employee.id,
+                        'employee_name': employee.name,
+                        'date': date,
+                        'entry_time': det_in.entry,
+                        'exit_time': det_out.out,
+                        'total_time': total_time_str,
+                    })
+                elif det_in:
+                    attendance_data.append({
+                        'employee_id': employee.id,
+                        'employee_name': employee.name,
+                        'date': date,
+                        'entry_time': det_in.entry,
+                        'exit_time': None,
+                        'total_time': None,
+                    })
+                elif det_out:
+                    attendance_data.append({
+                        'employee_id': employee.id,
+                        'employee_name': employee.name,
+                        'date': date,
+                        'entry_time': None,
+                        'exit_time': det_out.out,
+                        'total_time': None,
+                    })
+
+            # Sort attendance data by date
+            attendance_data.sort(key=lambda x: x['date'])
+
+            context = {
+                'attendance_data': attendance_data,
+                'start_date': start_date_formatted,
+                'end_date': end_date_formatted,
+            }
+        else:
+            # Dates are not selected or empty, no change occurs
+            context = {}
+
+        if 'generate_pdf' in request.GET:
+            # Generate PDF
+            template = get_template('app/personal_report.html')
+            html = template.render(context)
+            result = BytesIO()
+
+            # Convert HTML to PDF
+            pisaStatus = pisa.CreatePDF(html, dest=result)
+            if pisaStatus.err:
+                return HttpResponse('Error generating PDF')
+
+            # Create HTTP response with PDF attachment
+            response = HttpResponse(result.getvalue(), content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="personal_report.pdf"'
+
+            return response
+
+    else:
+        # Handle GET request with initial date range selection form
+        context = {}
+
+    return render(request, 'app/personal_report2.html', context)
 # @login_required(login_url='/app/login/')
 # def generate_pdf4(request):
 #     # get the template
